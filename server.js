c02a0b61572c5ba4b2105131a0338fb45e3e4b91
Vincent <vincent00lav@outlook.com>
2025-06-20 14:17:39 +0200

    Mise en place des routes / mise en place de express / mise en place du script de lancement de l'application

---
+++ server.js
@@ -0,0 +1,25 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+
+app.use(bodyParser.json());
+app.get("/api/v1/whisper", (req, res) => {
+  res.json([]);
+});
+app.get("/api/v1/whisper/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  res.json({ id });
+});
+
+app.post("/api/v1/whisper", (req, res) => {
+  res.status(201).json(req.body);
+});
+app.put("/api/v1/whisper/:id", (req, res) => {
+  res.sendStatus(200);
+});
+app.delete("/api/v1/whisper/:id", (req, res) => {
+  res.sendStatus(200);
+});
+
+export { app };
